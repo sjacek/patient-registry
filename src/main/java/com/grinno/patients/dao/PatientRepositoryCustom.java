d6450cb6c605e745ec2312736f9a5cf0264bdb7e
Jacek Sztajnke <jacek.sztajnke@gmail.com>
2016-11-07 17:07:13 +0100

    Updated copyright info

--- src/main/java/com/grinno/patients/dao/PatientRepositoryCustom.java
+++ src/main/java/com/grinno/patients/dao/PatientRepositoryCustom.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Pivotal Software, Inc.
+ * Copyright (C) 2016 Jacek Sztajnke
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,9 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.grinno.patients.dao;
-
-import com.grinno.patients.model.Patient;
-import java.util.List;
 
 /**
  *
