4a5ca09f862d10227ebca4d417804a8781a16999
Jacek Sztajnke <jacek.sztajnke@gmail.com>
2016-11-20 20:46:13 +0100

    Enabled https

--- src/main/java/com/grinno/patients/config/WebConfig.java
+++ src/main/java/com/grinno/patients/config/WebConfig.java
@@ -12,8 +12,41 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.samskivert.mustache.Mustache;
 
 import ch.ralscha.extdirectspring.util.JsonHandler;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@ConfigurationProperties(prefix = "server")
+class ServerProperties {
+
+    private Integer port;
+    private Integer httpPort;
+
+    public Integer getPort() {
+        return this.port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public Integer getHttpPort() {
+        return this.httpPort;
+    }
+
+    public void setHttpPort(Integer httpPort) {
+        this.httpPort = httpPort;
+    }
+}
 
 @Configuration
+@EnableConfigurationProperties(ServerProperties.class)
 public class WebConfig {
 
     @Bean
@@ -40,5 +73,36 @@ public class WebConfig {
     @Bean
     public Mustache.Compiler mustacheCompiler() {
         return Mustache.compiler();
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    @Autowired
+    ServerProperties serverProperties;
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(serverProperties.getHttpPort());
+        connector.setSecure(false);
+        connector.setRedirectPort(serverProperties.getPort());
+
+        return connector;
     }
 }
