f2360dbdd43da1e70ad0c3f7beb157ebbf58e868
Jacek Sztajnke <jacek.sztajnke@gmail.com>
2016-11-25 10:03:01 +0100

    gpl

--- src/main/sencha/ext/licenses/file-header.js
+++ src/main/sencha/ext/licenses/file-header.js
@@ -5,14 +5,17 @@ Copyright (c) 2011-2016 Sencha Inc
 
 Contact:  http://www.sencha.com/contact
 
-Commercial Usage
-Licensees holding valid commercial licenses may use this file in accordance with the Commercial
-Software License Agreement provided with the Software or, alternatively, in accordance with the
-terms contained in a written agreement between you and Sencha.
+GNU General Public License Usage
+This file may be used under the terms of the GNU General Public License version 3.0 as
+published by the Free Software Foundation and appearing in the file LICENSE included in the
+packaging of this file.
+
+Please review the following information to ensure the GNU General Public License version 3.0
+requirements will be met: http://www.gnu.org/copyleft/gpl.html.
 
 If you are unsure which license is appropriate for your use, please contact the sales department
 at http://www.sencha.com/contact.
 
-Version: 6.2.0.981 Build date: 2016-08-31 14:41:53 (08dbbd0ec0b8bc0e014d725fdb7d9650d510b343)
+Version: 6.2.0.981 Build date: 2016-08-31 14:49:44 (08dbbd0ec0b8bc0e014d725fdb7d9650d510b343)
 
 */
