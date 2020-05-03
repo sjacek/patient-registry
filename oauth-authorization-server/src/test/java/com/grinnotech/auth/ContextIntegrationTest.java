4b93089a795561386bd0c3b140459ba72c94cddb
Jacek Sztajnke <jacek.sztajnke@gmail.com>
2020-05-03 20:03:03 +0200

    some fixes to gradle in oauth-authorization module

--- oauth-authorization-server/src/test/java/com/grinnotech/auth/ContextIntegrationTest.java
+++ oauth-authorization-server/src/test/java/com/grinnotech/auth/ContextIntegrationTest.java
@@ -1,11 +1,9 @@
 package com.grinnotech.auth;
 
-import org.junit.jupiter.apikeycloakVersion.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import com.grinnotech.auth.AuthorizationServerApp;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = { AuthorizationServerApp.class })
